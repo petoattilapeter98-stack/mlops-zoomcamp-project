667a1d0ba3635bf5d3db1914c7aa0e5d85404abe
petoattilapeter98-stack <petoattilapeter98@gmail.com>
2025-12-09 07:02:52 +0000

    Save progress of homework

--- 02-experiment-tracking/homework/hpo.py
+++ 02-experiment-tracking/homework/hpo.py
@@ -34,11 +34,14 @@ def run_optimization(data_path: str, num_trials: int):
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
     def objective(params):
-
-        rf = RandomForestRegressor(**params)
-        rf.fit(X_train, y_train)
-        y_pred = rf.predict(X_val)
-        rmse = root_mean_squared_error(y_val, y_pred)
+        
+        with mlflow.start_run():
+            rf = RandomForestRegressor(**params)
+            rf.fit(X_train, y_train)
+            y_pred = rf.predict(X_val)
+            rmse = root_mean_squared_error(y_val, y_pred)
+            mlflow.log_param("rmse", rmse)
+            mlflow.log_metric("rmse", rmse)
 
         return {'loss': rmse, 'status': STATUS_OK}
 
