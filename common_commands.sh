7b17a659d6a871322821e8617745a59f299c601a
petoattilapeter98-stack <petoattilapeter98@gmail.com>
2025-12-09 06:45:25 +0000

    Save progress

--- common_commands.sh
+++ common_commands.sh
@@ -9,4 +9,4 @@ jupyter notebook
 # MLflow Server
 cd 02-experiment-tracking
 conda activate my-exp-tr
-mlflow server --backend-store-uri sqlite:///mlflow.db
+mlflow server --backend-store-uri sqlite:///mlflow.db --default-artifact-root ./artifacts_local
