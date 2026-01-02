d792522cfd5f9dd205142f6c8a1e49251c8df79a
Peter Peto <petoattilapeter98@gmail.com>
2026-01-02 09:25:36 +0100

    Save progress after homework 01

--- 06-best-practises/homework/batch.py
+++ 06-best-practises/homework/batch.py
@@ -5,21 +5,7 @@ import sys
 import pickle
 import pandas as pd
 
-
-year = int(sys.argv[1])
-month = int(sys.argv[2])
-
-input_file = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
-output_file = f'output/yellow_tripdata_{year:04d}-{month:02d}.parquet'
-
-
-with open('model.bin', 'rb') as f_in:
-    dv, lr = pickle.load(f_in)
-
-
-categorical = ['PULocationID', 'DOLocationID']
-
-def read_data(filename):
+def read_data(filename, categorical):
     df = pd.read_parquet(filename)
     
     df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
@@ -31,22 +17,38 @@ def read_data(filename):
     
     return df
 
+def main(year, month):
+    year = int(sys.argv[1])
+    month = int(sys.argv[2])
 
-df = read_data(input_file)
-df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
-
-
-dicts = df[categorical].to_dict(orient='records')
-X_val = dv.transform(dicts)
-y_pred = lr.predict(X_val)
+    input_file = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+    output_file = f'output/yellow_tripdata_{year:04d}-{month:02d}.parquet'
 
 
-print('predicted mean duration:', y_pred.mean())
+    with open('model.bin', 'rb') as f_in:
+        dv, lr = pickle.load(f_in)
 
 
-df_result = pd.DataFrame()
-df_result['ride_id'] = df['ride_id']
-df_result['predicted_duration'] = y_pred
+    categorical = ['PULocationID', 'DOLocationID']
+
+    df = read_data(input_file, categorical)
+    df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
 
 
-df_result.to_parquet(output_file, engine='pyarrow', index=False)
+    dicts = df[categorical].to_dict(orient='records')
+    X_val = dv.transform(dicts)
+    y_pred = lr.predict(X_val)
+
+
+    print('predicted mean duration:', y_pred.mean())
+
+
+    df_result = pd.DataFrame()
+    df_result['ride_id'] = df['ride_id']
+    df_result['predicted_duration'] = y_pred
+
+
+    df_result.to_parquet(output_file, engine='pyarrow', index=False)
+
+if __name__ == '__main__':
+    main(year=sys.argv[1], month=sys.argv[2])
